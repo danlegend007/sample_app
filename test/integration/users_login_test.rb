6e6c26170a149c6a09a588d54804016ecaab80a7
DaN <densherb95@gmail.com>
2023-01-03 02:13:49 +0600

    Improve Implement basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
+class UsersLogin < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:denis)
+  end
+end
+
+class InvalidPasswordTest < UsersLogin
+
+  test "login path" do
+    get login_path
+    assert_template 'sessions/new'
   end
 
   test "login with valid email/invalid password" do
@@ -18,24 +26,52 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+end
 
-  test "login with valid information followed by logout" do
-    post login_path, params: {session: { email: @user.email,
-                                         password: 'password' } }
+class ValidLogin < UsersLogin
+
+  def setup
+    super
+post login_path, params: { session: { email:    @user.email,
+                                      password: 'password' } }   
+  end
+end
+
+class ValidLoginTest < ValidLogin
+
+  test "valid login" do
     assert is_logged_in?
-assert_redirected_to @user
+    assert_redirected_to @user
+  end
+
+  test "redirect after login" do
     follow_redirect!
     assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", login_path, count: 0      
+    assert_select "a[href=?]", logout_path  
     assert_select "a[href=?]", user_path(@user)
-    delete logout_path
+  end
+end
+
+class Logout < ValidLogin 
+  def setup
+    super
+delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+
+  test "successful logout" do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
+  end
+  
+  test "redirect after logout" do
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
-end
-end
+    assert_select "a[href=?]", logout_path,      count: 0  
+    assert_select "a[href=?]", users_path(@user), count: 0
+  end
+end  
