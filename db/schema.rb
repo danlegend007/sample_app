5394211aa7f1d255020216c649cef0211faf935f
Vadim Terentyev <vterentyev@humans.net>
2023-01-04 23:28:34 +0600

    add changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_27_144303) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.text "name"
+    t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
+    t.text "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
