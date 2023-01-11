1450e38273907967b03cf15ac97d022db601df49
DaN <densherb95@gmail.com>
2023-01-12 02:24:21 +0600

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless @user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -64,14 +64,14 @@ class UsersController < ApplicationController
     end
   end
     
-# Confirms the correct user.
+ # Confirms the correct user.
  def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
-  end
+  @user = User.find(params[:id])
+  redirect_to(root_url, status: :see_other) unless current_user?(@user)
+end
 
 # Confirms an admin user.
-  def admin_user
-        redirect_to(root_url, status: :see_other) unless current_user.admin?
-  end
+def admin_user
+  redirect_to(root_url, status: :see_other) unless current_user.admin?
+end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,10 +1,10 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-def setup
-  @user = User.new(name: "Example User", email: "user@example.com",
-                   password: "foobar", password_confirmation: "foobar")
-end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
+  end
 
  test "should ve valid" do
   assert @user.valid?
@@ -65,4 +65,11 @@ test "authenticated? should return false for a user with nil digest" do
   assert_not @user.authenticated?(:remember, '')
 end
 
+test "associated microposts should be destroyed" do
+  @user.save
+  @user.microposts.create!(content: "Lorem ipsum")
+  assert_difference 'Micropost.count', -1 do
+    @user.destroy
+  end
+end
 end
