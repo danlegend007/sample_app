ea59dc281271b09e53161f1e1485c8b9c7e7037e
DaN <densherb95@gmail.com>
2023-01-13 03:01:33 +0600

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
 
@@ -37,7 +38,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
-# Handle a successful update.
+  # Handle a successful update.
     else
       render 'edit', status: :unprocessable_entity
     end
@@ -49,13 +50,27 @@ class UsersController < ApplicationController
     redirect_to users_url, status: :see_other
   end
   
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow', status: :unprocessable_entity
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow', status: :unprocessable_entity
+  end
+
   private
 
   def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
     
-# Confirms a logged-in user.
+  # Confirms a logged-in user.
   def logged_in_user
     unless logged_in?
         store_location
@@ -64,14 +79,14 @@ class UsersController < ApplicationController
     end
   end
     
- # Confirms the correct user.
- def correct_user
-  @user = User.find(params[:id])
-  redirect_to(root_url, status: :see_other) unless current_user?(@user)
-end
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
 
-# Confirms an admin user.
-def admin_user
-  redirect_to(root_url, status: :see_other) unless current_user.admin?
-end
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,70 +6,102 @@ class UserTest < ActiveSupport::TestCase
                      password: "foobar", password_confirmation: "foobar")
   end
 
- test "should ve valid" do
-  assert @user.valid?
- end
+  test "should ve valid" do
+    assert @user.valid?
+  end
 
- test "name should be present" do
-  @user.name = "a" * 51
-  assert_not @user.valid?
- end
+  test "name should be present" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
  
-test "email should be present" do
-  @user.email = "a" * 244 + "@example.com"
-  assert_not @user.valid?
-end
+  test "email should be present" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 
-test "email validation should accept valid addresses" do
-  valid_addresses = %W[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                       first.last@foo.jp alice+bob@baz.cn]
-  valid_addresses.each do |valid_address|
-    @user.email = valid_address
-    assert @user.valid?, "#{valid_address.inspect} should be valid"
- end
-end
+  test "email validation should accept valid addresses" do
+    valid_addresses = %W[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                        first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+  end
+  end
 
-test "email validation should reject invalid addresses" do
-    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                         foo@bar_baz.com foo@bar+baz.com
-                        foo@bar..com]
-    invalid_addresses.each do |invalid_address|
-      @user.email = invalid_address
-      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+  test "email validation should reject invalid addresses" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com
+                          foo@bar..com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+      end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lowercase" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end 
+
+  test "should follow and unfollow a user" do
+      denis = users(:denis)
+      archer  = users(:archer)
+      assert_not denis.following?(archer)
+      denis.follow(archer)
+      assert denis.following?(archer)
+      assert archer.followers.include?(denis)
+      denis.unfollow(archer)
+      assert_not denis.following?(archer)
+      # Users can't follow themselves.
+      denis.follow(denis)
+      assert_not denis.following?(denis)
+  end
+
+  test "feed should have the right posts" do
+    denis = users(:denis)
+    archer  = users(:archer)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert denis.feed.include?(post_following)
+    end
+    # Self-posts for user with followers
+    denis.microposts.each do |post_self|
+      assert denis.feed.include?(post_self)
+    end
+    # Posts from non-followed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not denis.feed.include?(post_unfollowed)
     end
   end
-
-test "email addresses should be unique" do
-  duplicate_user = @user.dup
-  @user.save
-  assert_not duplicate_user.valid?
- end
-
-test "email addresses should be saved as lowercase" do
-  mixed_case_email = "Foo@ExAMPle.CoM"
-  @user.email = mixed_case_email
-  @user.save
-  assert_equal mixed_case_email.downcase, @user.reload.email
-end
-
-test "password should be present (nonblank)" do
-  @user.password = @user.password_confirmation = " " * 6
-  assert_not @user.valid?
-end
-test "password should have a minimum length" do
-  @user.password = @user.password_confirmation = "a" * 5
-  assert_not @user.valid?
-end
-
-test "authenticated? should return false for a user with nil digest" do
-  assert_not @user.authenticated?(:remember, '')
-end
-
-test "associated microposts should be destroyed" do
-  @user.save
-  @user.microposts.create!(content: "Lorem ipsum")
-  assert_difference 'Micropost.count', -1 do
-    @user.destroy
-  end
-end
 end
