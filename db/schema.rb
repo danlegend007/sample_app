94b8b596f0badcd6e05c563398cc90a693e3c731
DaN <densherb95@gmail.com>
2023-01-21 20:50:14 +0600

    Rebase on Postgres with error

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_01_11_202952) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
+    t.text "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
@@ -23,26 +25,26 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_202952) do
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.text "key", null: false
+    t.text "filename", null: false
+    t.text "content_type"
     t.text "metadata"
-    t.string "service_name", null: false
+    t.text "service_name", null: false
     t.bigint "byte_size", null: false
-    t.string "checksum"
+    t.text "checksum"
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
+    t.text "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -64,15 +66,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_11_202952) do
     t.text "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "password_digest"
-    t.string "remember_digest"
+    t.text "password_digest"
+    t.text "remember_digest"
     t.boolean "admin", default: false
-    t.string "activation_digest"
+    t.text "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
-    t.string "reset_digest"
+    t.text "reset_digest"
     t.datetime "reset_sent_at"
-
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
